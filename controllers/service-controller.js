3572b15a2f9489c65da03465cddf2350f643e6ae
Krishna Sheth <krishna.sheth@inbox-infotech.com>
2025-08-06 18:32:55 +0530

    changes in file and add blog image

--- controllers/service-controller.js
+++ controllers/service-controller.js
@@ -119,15 +119,15 @@ const updateServices = async (req, res) => {
     const serviceId = req.params.id;
     const updateData = req.body;
 
-    if( req.files && req.files.service_image && req.files.service_image[0]){
-    updateData.service_image = `/uploads/services/${req.files.service_image[0].filename}`;
-    console.log("Uploaded files:", req.files);
-  }
+    if (req.files && req.files.service_image && req.files.service_image[0]) {
+      updateData.service_image = `/uploads/services/${req.files.service_image[0].filename}`;
+      console.log("Uploaded files:", req.files);
+    }
 
-  //  // Handle new uploaded single image
-  //   if (req.file) {
-  //     updateData.service_image = `/uploads/serviceimage/${req.file.filename}`;
-  //   }
+    //  // Handle new uploaded single image
+    //   if (req.file) {
+    //     updateData.service_image = `/uploads/serviceimage/${req.file.filename}`;
+    //   }
 
     //   // Handle new uploaded multiple images
     //   if (req.files && req.files.length > 0) {
